6e2bc5dff4dd65e53b2f8cd6bac2b1cd4f14ff12
M42 <mromang08@gmail.com>
2015-03-17 20:19:47 +0100

    Without GUI!!!!!!

--- agent/environment.cpp
+++ agent/environment.cpp
@@ -18,13 +18,13 @@ Environment::Environment(ifstream &infile)
 	string comment;
 	getline(infile, comment);
 	infile >> agentPosX_ >> agentPosY_ >> Grow_prob_ >> randomSeed_;
-    agentOriented_=0; //Siempre mira hacia el norte
-
+        agentOriented_=0; //Siempre mira hacia el norte
+    
 	for (int row=0; row<MAZE_SIZE; row+=1)
-	{
+            {
 		for (int col=0; col<MAZE_SIZE; col+=1)
-		{
-            maze2_[row][col]=0;
+                    {
+                        maze2_[row][col]=0;
 			char c;
 			infile >> c;
 			if (c == MAP_ROAD) {
@@ -52,201 +52,10 @@ int Environment::TrufaAmount(int x, int y) const
 	else return maze_[x][y];
 }
 // -----------------------------------------------------------
-void Environment::Show(int w,int h) const
-{
+void Environment::Show(int w,int h) const {
 	const static float trufa_level=50.0;
 	float length=(float)(w>h?h:w)/(float)(MAZE_SIZE+4);
 	float size=0.8*length;
-	for (int row=0; row<MAZE_SIZE; row+=1)
-	{
-		for (int col=0; col<MAZE_SIZE; col+=1)
-		{
-			float x=(col-MAZE_SIZE/2)*length*2+length,y=(MAZE_SIZE/2-row)*length*2-length;
-			if (maze_[row][col] == OBSTACLE){
-				//cout << "XXXXXX";
-
-				glBegin(GL_POLYGON);
-					glColor3f(0.5,0.25,0.0);
-					glVertex2f(x-length,y-length);	glVertex2f(x+length,y-length);
-					glVertex2f(x+length,y+length);	glVertex2f(x-length,y+length);
-				glEnd();
-			}
-			else
-			{
-				char symbol = ' ';
-   				if (row == agentPosX_ && col == agentPosY_){
-					symbol = '=';
-					switch(preAction_){
-						case Agent::actIDLE:
-   						    switch (agentOriented_){
-   						      case 0: // Orientacion Norte
-							         glBegin(GL_POLYGON);
-   						             glColor3f(0.0,0.0,1.0);
-								     glVertex2f(x,y+size);
-								     glVertex2f(x+size,y-size);	glVertex2f(x-size,y-size);
-							         glEnd();
-							         break;
-                              case 1: // Orientacion Este
-							         glBegin(GL_POLYGON);
-   						             glColor3f(0.0,0.0,1.0);
-								     glVertex2f(x+size,y);
-								     glVertex2f(x-size,y-size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 2: // Orientacion Sur
-							         glBegin(GL_POLYGON);
-   						             glColor3f(0.0,0.0,1.0);
-								     glVertex2f(x,y-size);
-								     glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 3: // Orientacion Oeste
-							         glBegin(GL_POLYGON);
-   						             glColor3f(0.0,0.0,1.0);
-								     glVertex2f(x-size,y);
-								     glVertex2f(x+size,y-size);	glVertex2f(x+size,y+size);
-								     glEnd();
-								     break;
-   						    }
-							break;
-						case Agent::actSNIFF:
-							glBegin(GL_POLYGON);
-								glColor3f(1.0,0.8,0.3);
-								glVertex2f(x-size,y-size);	glVertex2f(x+size,y-size);
-								glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-							glEnd();
-							break;
-						case Agent::actEXTRACT:
-							glBegin(GL_POLYGON);
-								glColor3f(1.0,0.3,0.8);
-								glVertex2f(x-size,y-size);	glVertex2f(x+size,y-size);
-								glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-							glEnd();
-							break;
-						case Agent::actFORWARD:
-							if(bump_){
-								glColor3f(1.0,0.0,0.0);
-							}
-							else{
-								glColor3f(0.0,1.0,0.0);
-							}
-   						    switch (agentOriented_){
-   						      case 0: // Orientacion Norte
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x,y+size);
-								     glVertex2f(x+size,y-size);	glVertex2f(x-size,y-size);
-							         glEnd();
-							         break;
-                              case 1: // Orientacion Este
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x+size,y);
-								     glVertex2f(x-size,y-size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 2: // Orientacion Sur
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x,y-size);
-								     glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 3: // Orientacion Oeste
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x-size,y);
-								     glVertex2f(x+size,y-size);	glVertex2f(x+size,y+size);
-								     glEnd();
-								     break;
-   						    }
-							break;
-						case Agent::actTURN_L:
-						case Agent::actTURN_R:
-								glColor3f(0.0,1.0,0.0);
-   						    switch (agentOriented_){
-   						      case 0: // Orientacion Norte
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x,y+size);
-								     glVertex2f(x+size,y-size);	glVertex2f(x-size,y-size);
-							         glEnd();
-							         break;
-                              case 1: // Orientacion Este
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x+size,y);
-								     glVertex2f(x-size,y-size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 2: // Orientacion Sur
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x,y-size);
-								     glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-								     glEnd();
-								     break;
-                              case 3: // Orientacion Oeste
-							         glBegin(GL_POLYGON);
-								     glVertex2f(x-size,y);
-								     glVertex2f(x+size,y-size);	glVertex2f(x+size,y+size);
-								     glEnd();
-								     break;
-   						    }
-							break;
-					}
-				}
-				if (maze2_[row][col]==0){
-                    glBegin(GL_POLYGON);
-                        glColor3f(((float)(trufa_level-maze_[row][col]))/trufa_level,((float)(trufa_level-maze_[row][col]))/trufa_level,((float)(trufa_level-maze_[row][col]))/trufa_level);
-                        glVertex2f(x-length,y-length);	glVertex2f(x+length,y-length);
-                        glVertex2f(x+length,y+length);	glVertex2f(x-length,y+length);
-                    glEnd();
-                }
-                else if (maze2_[row][col]==1){
-                    glBegin(GL_POLYGON);
-                        glColor3f(1.0,1.0,1.0);
-                        glVertex2f(x-size,y-size);	glVertex2f(x+size,y-size);
-                        glVertex2f(x+size,y+size);	glVertex2f(x-size,y+size);
-                    glEnd();
-                    glBegin(GL_POLYGON);
-                        glColor3f(0.0,0.0,0.0);
-                        glVertex2f(x-length,y-length);	glVertex2f(x+length,y-length);
-                        glVertex2f(x+length,y+length);	glVertex2f(x-length,y+length);
-                    glEnd();
-                }
-                else if (maze2_[row][col]==2){
-                    glBegin(GL_POLYGON);
-                        glColor3f(1.0,1.0,1.0);
-                        glVertex2f(x-length/2,y-length/2);	glVertex2f(x+length/2,y-length/2);
-                        glVertex2f(x+length/2,y+length/2);	glVertex2f(x-length/2,y+length/2);
-                    glEnd();
-                    glBegin(GL_POLYGON);
-                        glColor3f(0.0,0.0,0.0);
-                        glVertex2f(x-length,y-length);	glVertex2f(x+length,y-length);
-                        glVertex2f(x+length,y+length);	glVertex2f(x-length,y+length);
-                    glEnd();
-                }
-                else if (maze2_[row][col]==3){
-                    glBegin(GL_POLYGON);
-                        glColor3f(0.0,0.0,0.0);
-                        glVertex2f(x-length,y-length);	glVertex2f(x+length,y-length);
-                        glVertex2f(x+length,y+length);	glVertex2f(x-length,y+length);
-                    glEnd();
-                }
-                else {
-                    glBegin(GL_POLYGON);
-                        glColor3f(1.0,1.0,1.0);
-                        for(int j=0;j<100;j++)
-                        {
-                            float angle = ((2*3.1415926535897*j)/100);
-
-                           glVertex2f(x+size*cos(angle),y+size*sin(angle));
-                        }
-                    glEnd();
-
-                }
-				//cout << symbol;
-				//cout << setw(4) << maze_[row][col];
-				//cout << symbol;
-
-			}
-		}// for - col
-		//cout << endl;
-	}// for - row
 }
 // -----------------------------------------------------------
 void Environment::Change(const RandomNumberGenerator &rng)
--- agent/main.cpp
+++ agent/main.cpp
@@ -1,11 +1,131 @@
-#include "GUI.h"
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+#include<iostream>
+#include <unistd.h>
+using namespace std;
+#include<stdlib.h>
+#include"random_num_gen.h"
+#include"environment.h"
+#include"agent.h"
+#include"evaluator.h"
+#include"string.h"
+#include<string>
 
-int main(int argc, char *argv[]){
-	return GUI::startDraw(argc,argv);
+const int NEWGAME_BTN_ID=102,SELECTMAP_BTN_ID=103,DISPLAY_BTN_ID=104,NEWMAP_BTN_ID=105;
+const int NEWMAP_OK_BTN_ID=202,NEWMAP_CANCEL_BTN_ID=203;
+const int SELECTMAP_SELECT_ID=302,SELECTMAP_CANCEL_BTN_ID=303;
+const int DO_ONE_STEP_BTN_ID=402,DO_ONE_RUN_BTN_ID=403,NEXT_RUN_BTN_ID=404,DO_ALL_RUN_BTN_ID=405;
+const int ERROR_OK_BTN_ID=502;
+
+string file_name_input="agent.map";
+char life_time_input[1000]="2000",total_runs_input[1000]="10";
+char display_step[20]="100",display_time[20]="10";
+char life_time_temp[1000],total_runs_temp[1000],file_name_temp[1000];
+int tx,ty,tw,th;
+float magnification_x,magnification_y;
+long long life_time=2000,current_time,total_runs=10,current_run,Trufa_Collected,consumed_energy;
+long long total_collect,total_consumed_energy;
+char time_step_msg[100],action_msg[100],Trufa_Collected_msg[100],consumed_energy_msg[100];
+
+Environment *env=NULL;
+Agent *agent=NULL;
+RandomNumberGenerator *rng=NULL;
+Evaluator *evaluator=NULL;
+Agent::ActionType action=Agent::actIDLE;
+ifstream fin;
+
+
+void doOneStep();
+void doOneRun();
+void doAllRun();
+void nextRun();
+void newGame();
+
+
+void doOneStep(){
+    if(current_time<life_time){
+        env->Change(*rng);
+        agent->Perceive(*env);
+
+        action = agent->Think();
+        env->AcceptAction(action);
+        evaluator->Eval(action,*env);
+
+        Trufa_Collected=env->TrufaCollected();
+        consumed_energy=evaluator->ConsumedEnergy();
+
+        ++current_time;
+        if(current_time==life_time){//¤@½üµ²§ô
+            total_collect+=Trufa_Collected;
+            total_consumed_energy+=consumed_energy;
+        }
+    }
 }
 
-#include <pthread.h>
-void junk() {
-    int i;
-    i=pthread_getconcurrency();
-};
+void doOneRun(){
+    while(current_time<life_time){
+        doOneStep();
+    }
+}
+
+void doAllRun(){
+    doOneRun();
+    while(current_run<total_runs){
+        nextRun();
+        doOneRun();
+    }
+}
+
+void nextRun(){
+    if(current_run<total_runs){
+        ++current_run;
+        newGame();
+    }
+}
+
+void newGame(){
+    fin.close();
+    fin.clear();
+    delete env;
+    delete agent;
+    delete rng;
+    delete evaluator;
+
+    fin.open(file_name_input.c_str());
+    if(fin){
+        current_time=0;
+        Trufa_Collected=0;
+        consumed_energy=0;
+        env=new Environment(fin);
+        rng=new RandomNumberGenerator(env->RandomSeed()+current_run);
+        evaluator=new Evaluator();
+        agent=new Agent();
+    }
+    else{
+        fin.clear();
+        char temp[1000]={"map/"};
+        strcat(temp,file_name_input.c_str());
+        fin.open(temp);
+        if(fin){
+            current_time=0;
+            Trufa_Collected=0;
+            consumed_energy=0;
+            env=new Environment(fin);
+            rng=new RandomNumberGenerator(env->RandomSeed()+current_run);
+            evaluator=new Evaluator();
+            agent=new Agent();
+        }
+        else{
+        }
+    }
+}
+
+
+int main(int argc, char* argv[]){
+    file_name_input = argv[1];
+    newGame();
+    doOneRun(); 
+    cout << Trufa_Collected << endl;
+}
+
