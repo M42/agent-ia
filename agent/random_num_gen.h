2481ef4ea9358b4493d901bbb9696013808decc8
M42 <mromang08@gmail.com>
2015-03-17 20:41:47 +0100

    Cleans file random_num_gen

--- agent/random_num_gen.h
+++ agent/random_num_gen.h
@@ -1,28 +1,26 @@
-
 #ifndef RANDOMNUMGENERATOR__
 #define RANDOMNUMGENERATOR__
 
 #include <vector>
 using std::vector;
+using std::cout;
 
-// -----------------------------------------------------------
-//				class RandomNumberGenerator
-// -----------------------------------------------------------
-class RandomNumberGenerator
-{
+
+class RandomNumberGenerator {
 public:
-	RandomNumberGenerator(int seed);
-	int RandomValue() const { 
- 		if (index_ >= rand_num_.size())
- 		{
- 			cout << "Insufficient random values!"; exit(1);
- 		}
-   		return rand_num_[index_++]; 
-   }
+    RandomNumberGenerator(int seed);
+    int RandomValue() const { 
+        if (index_ >= rand_num_.size()) {
+                cout << "Insufficient random values!"; exit(1);
+        }
+        return rand_num_[index_++]; 
+    }
+
 private:	
-	vector<int> rand_num_;
-	mutable int	index_;
+    vector<int> rand_num_;
+    mutable int	index_;
+
 };
-// -----------------------------------------------------------
+
 
 #endif
