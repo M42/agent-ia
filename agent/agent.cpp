b01e069e57bed9c144f281bf9b6535a8673cff2f
M42 <mromang08@gmail.com>
2015-04-03 13:12:13 +0200

    Adds state updating functions

--- agent/agent.cpp
+++ agent/agent.cpp
@@ -22,24 +22,40 @@ void draw_map(Map& map) {
     }
 }
 
+
+Agent::Direction Agent::updateDir(Agent::Direction dir, Agent::ActionType action) {
+    if (action == actTURN_L)
+        switch (dir) {
+        case up:    return left;  break;
+        case left:  return down;  break;
+        case down:  return right; break;
+        case right: return up;    break;
+        }
+    if (action == actTURN_R)
+        switch (dir) {
+        case up:    return right; break;
+        case right: return down;  break;
+        case down:  return left;  break;
+        case left:  return up;    break;
+        }
+
+    return dir;
+}
+
+void Agent::updatePos(Agent::Direction dir) {
+    switch (dir) {
+    case up: posy--; break;
+    case down: posy++; break;
+    case left: posx--; break;
+    case right: posx++; break;
+    }
+}
+
 // -----------------------------------------------------------
 // Edited Code
 // -----------------------------------------------------------
 
-Agent::ActionType Agent::Think() {
-    // DEFINITIONS
-
-
-    // Iconic map, up to relative position
-    static Map mapa = Map(20, vector<bool>(20, false));
-    static int posx = 0;
-    static int posy = 0;
-
-    // Number of iterations
-    static int iteration = 0;
-
-
-    // REAL CODE
+Agent::ActionType Agent::Think_random() {
     ActionType accion;
 
     switch(rand()%5){
@@ -54,12 +70,27 @@ Agent::ActionType Agent::Think() {
     case 4: accion=actEXTRACT;
         break;
     }
+    
+    return accion;
+}
 
+Agent::ActionType Agent::Think() {
+    // DEFINITIONS
+    ActionType accion;
+
+    // Iconic map, up to relative position
+    static Map mapa = Map(20, vector<bool>(20, false));
+    static Direction dir = up;
+
+    // Number of iterations
+    static int iteration = 0;
 
     // POSCONDITIONS
     iteration++;
     if (iteration == 1000) draw_map(mapa);
-    return accion;
+
+    dir = updateDir(dir,accion);
+    accion = Think_random();
 }
 
 // -----------------------------------------------------------
--- agent/agent.h
+++ agent/agent.h
@@ -8,30 +8,44 @@ using namespace std;
 //				class Agent
 // -----------------------------------------------------------
 class Environment;
-class Agent
-{
-public:
-	Agent(){
+class Agent {
+ public:
+    Agent() {
         trufa_size_=-1;
         bump_=false;
-	}
 
-	enum ActionType
-	{
-	    actFORWARD,
-	    actTURN_L,
-	    actTURN_R,
-	    actSNIFF,
-	    actEXTRACT,
-	    actIDLE
-	};
+        posx = 0;
+        posy = 0;
+    }
 
-	void Perceive(const Environment &env);
-	ActionType Think();
-private:
-	int trufa_size_;
-	bool bump_;
+    enum ActionType {
+        actFORWARD,
+        actTURN_L,
+        actTURN_R,
+        actSNIFF,
+        actEXTRACT,
+        actIDLE
+    };
 
+    enum Direction {
+        up,
+        right,
+        down,
+        left
+    };
+
+    void Perceive(const Environment &env);
+    ActionType Think();
+    ActionType Think_random();
+    Direction updateDir(Direction, ActionType);
+    void updatePos(Direction);
+
+ private:
+    int trufa_size_;
+    bool bump_;
+
+    int posx;
+    int posy;
 };
 
 string ActionStr(Agent::ActionType);
